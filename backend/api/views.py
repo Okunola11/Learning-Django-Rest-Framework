9208b651f1d983db1e8d6752b479bf3b1255a6ff
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-04-24 19:47:42 +0100

    Derived JSON to dict using built in method 'model_to_dict', used Response and api_view from drf

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,28 +1,35 @@
-# from django.shortcuts import render
 import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+# from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by(
         "?").first()  # returns a random instance of 'Product'
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-                
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+
+        # built in method to avoid the manual process above, with options to select fields from the data
+        data = model_to_dict(model_data, fields=['id', 'title', 'sale_price'])
+
         # Serialization
         # model instance (model_data)
         # Turn to a Python dict()
         # Return JSON to my client
 
-    return JsonResponse(data)
+    return Response(data)
 
 
 # These are from the first lesson instance to learn basics
