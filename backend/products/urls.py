112635f095a0374d0c65dfee68d3049c00a21567
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-04-25 11:16:48 +0100

    Implemented users authentication and permissions

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_mixin_view),
-    path('<int:pk>/', views.product_mixin_view),
+    path('', views.ProductListCreateApiView.as_view()),
+    path('<int:pk>/', views.product_detail_view),
     path('update/<int:pk>/', views.product_update_view),
     path('delete/<int:pk>/', views.product_destroy_view),
 ]
