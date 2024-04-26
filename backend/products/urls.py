190078084690695f6131884370aec088c15da5e4
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-04-26 09:02:28 +0100

    Learnt more about serializers, adding urls, adding validators to fields

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListCreateApiView.as_view()),
-    path('<int:pk>/', views.product_detail_view),
-    path('update/<int:pk>/', views.product_update_view),
+    path('', views.ProductListCreateApiView.as_view(), name='product-list'),
+    path('<int:pk>/', views.product_detail_view, name='product-detail'),
+    path('update/<int:pk>/', views.product_update_view, name='product-edit'),
     path('delete/<int:pk>/', views.product_destroy_view),
 ]
