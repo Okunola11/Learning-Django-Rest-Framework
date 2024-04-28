5fc375af8f8873cfd9c75fb79703b3ac4b5d962b
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-04-28 08:02:43 +0100

    Enabled algolia to be used in the client of our API, making use of tagFilters and facetFilters for a robust search

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,10 +1,29 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from products.models import Product
 from products.serializers import ProductSerializer
 
+from . import client
+
 
 class SearchListView(generics.ListAPIView):
+    def get(self, request, *args, **kwargs):
+        user = None
+        if request.user.is_authenticated:
+            user = request.user.username
+        query = request.GET.get('q')
+        public = str(request.GET.get('public')) != '0'
+        tag = request.GET.get('tag') or None
+        print(user, query, public)
+        if not query:
+            return Response('', status=400)
+        result = client.perform_search(
+            query, tags=tag, user=user, public=public)
+        return Response(result)
+
+
+class DjangoSearchListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
